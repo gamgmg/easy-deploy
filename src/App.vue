19c497cc796773dc8cc17fbb8e36cc752ba8a0cf
黄嘉铭 <a347975891@qq.com>
2020-07-19 17:34:00 +0800

    8

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App7" />
+    <HelloWorld msg="Welcome to Your Vue.js App8" />
   </div>
 </template>
 
